2970e446b735f417dba447ec230cb36d6638a6cf
Abhijit Pattanik <abhijitlaptop@gmail.com>
2022-02-05 22:39:09 +0530

    ABHIJIT MADE THE CHANGES

---
+++ src/Multiplication.java
@@ -0,0 +1,11 @@
+
+public class Multiplication {
+	double num1,num2,mul;
+Multiplication(double num1,double num2)
+{
+	this.num1=num1;
+	this.num2=num2;
+	mul = num1*num2;
+	System.out.println(num1+"*"+num2+"="+mul);
+}
+}
